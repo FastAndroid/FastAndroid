9bf6ef85cd80c57344b84d1421b0de5e4c8437ee
kevindives <kevinsagm@gmail.com>
2021-11-26 09:12:40 -0500

    se sube preferencefragment

--- ecosantander/src/main/java/com/magicworld/ecosantander/preference/SettingsFragment.kt
+++ ecosantander/src/main/java/com/magicworld/ecosantander/preference/SettingsFragment.kt
@@ -5,11 +5,17 @@ import android.view.Menu
 import android.view.MenuInflater
 import androidx.preference.PreferenceFragmentCompat
 import com.magicworld.ecosantander.R
+import com.magicworld.ecosantander.main.MainActivity
 
 class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+        (activity as MainActivity?)?.showIcon()
     }
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, inflater)
